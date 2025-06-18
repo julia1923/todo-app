57db59336bd901f3ed20e85b9c20b85d0b53bea5
Julia <juliaf.nascimento65@gmail.com>
2025-06-18 18:32:10 -0300

    Inserindo lógica para excluir as tarefas

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = ({task, taskCompleted}) => {
+export const Todo = ({task, taskCompleted, deleteTodo}) => {
   return (
     <div className='Todo'>
       <p 
@@ -12,7 +12,7 @@ export const Todo = ({task, taskCompleted}) => {
         {task.task}</p>
       <div>
         <FontAwesomeIcon icon={faPenToSquare}/>
-        <FontAwesomeIcon icon={faTrash}/>
+        <FontAwesomeIcon icon={faTrash} cursor='pointer' onClick={() => deleteTodo(task.id)}/>
       </div>
     </div>
   )
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -6,28 +6,35 @@ uuidv4();
 
 export const TodoWrapper = () => {
 
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]); // Array para armazenamento de tarefas 
 
-  const addTodo = todo => {
+  const addTodo = todo => { // Função para a criação da tarefa 
     setTodos([...todos, {id: uuidv4(), task: todo, completed: false, isEditing: false}])
 
     console.log(todos)
   }
 
-  const taskCompleted = id => {
+  const taskCompleted = id => { // Função para marcar a tarefa como completa
     setTodos(todos.map(
     todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
+  }
+
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id !== id))
+
   }
 
   return (
     <div className='TodoWrapper'>
       <h1>Concretize suas tarefas!</h1>
         <TodoForm addTodo={addTodo}></TodoForm>
+
         {todos.map((todo, index) => (
           <Todo task={todo} key={index}
-          taskCompleted={taskCompleted}
+          taskCompleted={taskCompleted} deleteTodo={deleteTodo}
           />
         ))}
+
     </div>
   )
 }
